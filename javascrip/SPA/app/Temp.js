51b996e34320032e7dcc066b10c6344d3776a583
luis <deskwork@gmail.com>
2024-12-30 16:45:14 -0500

    segundo commit

--- javascrip/SPA/app/Temp.js
+++ javascrip/SPA/app/Temp.js
@@ -1,9 +1,7 @@
-import api from "./helpers/wp_api.js";
-import {ajax} from "./helpers/ajax.js";
+
 import { Loader } from "./components/loader.js";
 import { Header } from "./components/headers.js";
 import { Post } from "./components/Posts.js";
-import { PostCard } from "./components/postCards.js";
 import { Router } from "./components/Router.js";
 import { InfiniteScroll } from "./helpers/infiniteScroll.js";
 const $div=document.querySelector(".root");
